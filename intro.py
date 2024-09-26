968fbd25284e23908daed1837a13a634ee230c36
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-09-26 12:55:51 +0300

    intro to python

---
+++ intro.py
@@ -0,0 +1,38 @@
+name='ahmed'
+if name=='ahmed':
+      print("Hi Ahmed")
+else:
+
+    print("amuone")
+
+
+bio="My name is ahmed."
+
+bio2=("My name is ahmed."
+      "I works at iti")
+
+print(bio2)
+
+bio3=("My name is ahmed.\n"
+      "I works at iti")
+
+print(bio3)
+
+bio='''My name is ahmed.
+I works at iti
+I lives in cairo'''
+print(bio)
+
+"""this is a content --> as comment """  # this string is treated like a comment.
+
+
+name='ahmed'
+age=10
+g=9.8
+found=False
+
+s=input("plz enter space  ")
+print(s.isspace())
+
+
+
