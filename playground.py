fc2044fd3ff7e947e9fa9cc29b7c0462967f88bf
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-09-29 12:53:20 +0300

    dict, tuple and functions

---
+++ playground.py
@@ -0,0 +1,61 @@
+
+
+# generate list of number 1 --> 10 ?
+# nums = []
+# counter = 1
+# while counter < 11:
+#     nums.append(counter)
+#     counter += 1
+#
+# print(nums)
+
+
+
+
+# correct = False
+# for i in range(1, 4):
+#     password = input("Enter password: ")
+#     if password =='1234':
+#         print("Password is Correct")
+#         correct = True
+#         break
+#
+#     print("---- plz try again")
+#
+#
+# if i==3 and correct==False:
+#     print("--- account has been locked")
+
+
+
+
+
+
+
+
+# for i in range(1, 4):
+#     password = input("Enter password: ")
+#     if password =='1234':
+#         print("Password is Correct")
+#         break
+#     print("---- plz try again")
+# else:
+#     print("--- account has been locked")
+
+
+
+
+name=  'ahmed'
+if name:
+    pass
+
+print("-----")
+
+
+
+
+
+
+
+
+
