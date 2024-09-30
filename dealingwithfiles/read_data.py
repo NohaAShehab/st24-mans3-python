8dfc277381ee23534bbbb19930b4eb4b4e5bb288
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-09-30 15:54:55 +0300

    final operations

---
+++ dealingwithfiles/read_data.py
@@ -0,0 +1,17 @@
+
+#
+#
+# try:
+#     fileobj = open("students.txt", "r") # TextIOWrapper
+#
+# except Exception as e:
+#     print(e)
+#
+# else:
+#     print(fileobj)
+#     data  = fileobj.read() # read file content into one string
+#     print(data)
+#
+#     fileobj.close()
+
+"read file line b "
